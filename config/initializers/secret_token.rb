9d91d2c267c3854b03395dc84d31fe600bf7a361
Merrick Dennis <merrickd@verizon.net>
2014-02-12 14:57:33 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '893506be657237f65a3a731d61cc0fbb102a93dfce49ad72708d824a6451e677ab7986c4a53f10c41f5766aa40909151414c5adeefda2dca8e4e34fb0c6df2ba'
